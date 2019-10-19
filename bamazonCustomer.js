be8bf230c0e2db9ecd42637ab9f55953fff4d3e2
Elisabeth Loeser <19419418+ElliLoes@users.noreply.github.com>
2019-10-19 21:05:15 +1100

    Table layout improved

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,7 +18,7 @@ function queryAllProducts() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         for (var i = 0; i < res.length; i++) {
-            console.log(res[i].item_id + " || " + res[i].product_name + " || $" + res[i].price)
+            console.log("Product ID: " + res[i].item_id + " || Product Name: " + res[i].product_name + " || Product Price: $" + res[i].price)
         }
         console.log("----------------------------------------------------");
         makeOrder();
